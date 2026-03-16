596352c2782bcad213c11d1b495f564ebc73a3c1
Denis Okhrimenko <dochrimenko@mail.ru>
2026-03-16 19:55:53 +0300

    Подключил babel react compiler для автоматической генерации memo для компонентов

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,31 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react, { reactCompilerPreset } from '@vitejs/plugin-react'
+import babel from '@rolldown/plugin-babel'
+import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    // Стандартный плагин React (теперь на базе быстрого Oxc)
+    react(),
+
+    // Подключение Babel через Rolldown-плагин для компилятора
+    babel({
+      presets: [
+        reactCompilerPreset({
+          // Опции для компилятора (например, для React 18)
+          target: '18'
+        })
+      ],
+    }),
+    // Подключаем Tailwind v4
+    tailwindcss(),
+  ],
+  resolve: {
+    alias: {
+      // Настраиваем быстрый доступ к папке src через @
+      "@": path.resolve(__dirname, "./src"),
+    },
+  }
 })

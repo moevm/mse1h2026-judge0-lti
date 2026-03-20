973b87253c043c7f407c82d39b67a6d2526d2211
Denis Okhrimenko <dochrimenko@mail.ru>
2026-03-20 14:12:37 +0300

    fix: App теперь без Outlet, добавлены необходимые props для IDEpage

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
 import './App.css'
 
-import { BrowserRouter as Router, Routes, Route, Outlet } from "react-router-dom"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import {useState} from "react"
 
 import IDEPage from "./pages/IDEPage/IDEPage.tsx"
@@ -8,26 +8,13 @@ import TestPage from "./pages/TestPage/TestPage.tsx" // временная ст�
 import NotFoundPage from "./pages/NotFoundPage/NotFoundPage.tsx";
 import ForbiddenPage from "./pages/ForbiddenPage/ForbiddenPage.tsx";
 
-import Header from "./components/Header/Header.tsx"
-
 function App() {
   const [language, setLanguage] = useState("python");
+
   return (
       <Router>
         <Routes>
-          {/* Маршруты с хедером */}
-          <Route element={
-            <div className="app">
-              <Header language={language} setLanguage={setLanguage} />
-              <main className="content">
-                <Outlet />
-              </main>
-            </div>
-          }>
-            <Route path="/" element={<IDEPage language={language} />} />
-          </Route>
-
-          {/* 404 — без хедера */}
+          <Route path="/" element={<IDEPage language={language} setLanguage={setLanguage} />} />
           <Route path="/test" element={<TestPage />} />
           <Route path="/403" element={<ForbiddenPage />} />
           <Route path="*" element={<NotFoundPage />} />

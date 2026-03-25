814a1e4f00a2864f20eb2ca6dc831bdd7b4fa056
ShcherbakMaxim <maksim8305@mail.ru>
2026-03-25 22:09:54 +0300

    feat: log LTI launch data from e.moevm #52

--- backend/app/routers/lti.py
+++ backend/app/routers/lti.py
@@ -1,9 +1,17 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Request
 from fastapi.responses import RedirectResponse
+import logging
+
+logger = logging.getLogger(__name__)
 
 router = APIRouter(prefix="/lti", tags=["lti"])
 
 
 @router.post("/launch", summary="LTI Launch Endpoint")
-async def lti_launch():
+async def lti_launch(request: Request):
+    form_data = await request.form()
+
+    print("=== LTI LAUNCH ===")
+    for key, value in form_data.items():
+        print(f"{key}: {value}")
     return RedirectResponse(url="http://localhost", status_code=303)

538464ca848c52943a91cc57e822d8fee953057e
svvtkho <svtokhovad@gmail.com>
2026-03-26 16:22:42 +0300

    fix: now Solution model stores current_code

--- backend/app/database/models.py
+++ backend/app/database/models.py
@@ -1,3 +1,5 @@
+# @formatter:off
+
 import enum
 from sqlalchemy import *
 from sqlalchemy.orm import relationship, declarative_base
@@ -7,21 +9,21 @@ Base = declarative_base()
 
 
 class UserTypeEnum(enum.Enum):
-    admin = "admin"
+    admin   = "admin"
     student = "student"
     teacher = "teacher"
 
 
 class User(Base):
-    __tablename__ = "users"
+    __tablename__   = "users"
 
-    id = Column(BIGINT, primary_key=True, index=True)
-    username = Column(String(64), unique=True, nullable=False, index=True)
-    full_name = Column(String(128), nullable=False, index=True)
-    role = Column(ENUM(UserTypeEnum), nullable=False, index=True)
+    id              = Column(BIGINT, primary_key=True, index=True)
+    username        = Column(String(64), unique=True, nullable=False, index=True)
+    full_name       = Column(String(128), nullable=False, index=True)
+    role            = Column(ENUM(UserTypeEnum), nullable=False, index=True)
 
-    created_at = Column(TIMESTAMP(timezone=True), server_default=func.now())
-    updated_at = Column(TIMESTAMP(timezone=True), onupdate=func.now())
+    created_at      = Column(TIMESTAMP(timezone=True), server_default=func.now())
+    updated_at      = Column(TIMESTAMP(timezone=True), onupdate=func.now())
 
     solutions = relationship(
         "Solution", back_populates="user", cascade="all, delete-orphan"
@@ -29,44 +31,38 @@ class User(Base):
 
 
 class Module(Base):
-    __tablename__ = "modules"
+    __tablename__   = "modules"
 
-    id = Column(BIGINT, primary_key=True, index=True)
-    title = Column(String(128), index=True)
-    description = Column(Text, nullable=False)
+    id              = Column(BIGINT, primary_key=True, index=True)
+    title           = Column(String(128), index=True)
+    description     = Column(Text, nullable=False)
 
-    created_at = Column(TIMESTAMP(timezone=True), server_default=func.now())
-    updated_at = Column(TIMESTAMP(timezone=True), onupdate=func.now())
+    created_at      = Column(TIMESTAMP(timezone=True), server_default=func.now())
+    updated_at      = Column(TIMESTAMP(timezone=True), onupdate=func.now())
 
-    tasks = relationship("Task", back_populates="module")
-    task_orders = relationship(
+    tasks           = relationship("Task", back_populates="module")
+    task_orders     = relationship(
         "ModuleTaskOrder", back_populates="module", cascade="all, delete-orphan"
     )
 
 
 class Task(Base):
-    __tablename__ = "tasks"
+    __tablename__   = "tasks"
 
-    id = Column(BIGINT, primary_key=True, index=True)
-    module_id = Column(BIGINT, ForeignKey("modules.id"), index=True, nullable=False)
-    title = Column(String(128), index=True)
-    description = Column(Text, nullable=False)
-    timeout = Column(Integer, nullable=False, index=True)  # секунды
-    tests_pipeline = Column(
-        JSON, nullable=False
-    )  # {{title: ..., input: {...}, output: {...}}, {...}, {...}}
+    id              = Column(BIGINT, primary_key=True, index=True)
+    module_id       = Column(BIGINT, ForeignKey("modules.id"), index=True, nullable=False)
+    title           = Column(String(128), index=True)
+    description     = Column(Text, nullable=False)
+    timeout         = Column(Integer, nullable=False, index=True)   # секунды
+    tests_pipeline  = Column(JSON, nullable=False)                   # {{title: ..., input: {...}, output: {...}}, {...}, {...}}
 
-    created_at = Column(TIMESTAMP(timezone=True), server_default=func.now())
-    updated_at = Column(TIMESTAMP(timezone=True), onupdate=func.now())
+    created_at      = Column(TIMESTAMP(timezone=True), server_default=func.now())
+    updated_at      = Column(TIMESTAMP(timezone=True), onupdate=func.now())
 
-    module = relationship("Module", back_populates="tasks")
-    languages = relationship(
-        "Language", secondary="tasks_languages", back_populates="tasks"
-    )
-    solutions = relationship(
-        "Solution", back_populates="task", cascade="all, delete-orphan"
-    )
-    order_info = relationship(
+    module          = relationship("Module", back_populates="tasks")
+    languages       = relationship("Language", secondary="tasks_languages", back_populates="tasks")
+    solutions       = relationship("Solution", back_populates="task", cascade="all, delete-orphan")
+    order_info      = relationship(
         "ModuleTaskOrder",
         back_populates="task",
         uselist=False,
@@ -75,66 +71,58 @@ class Task(Base):
 
 
 class ModuleTaskOrder(Base):
-    __tablename__ = "module_tasks_order"
-    __table_args__ = (
+    __tablename__   = "module_tasks_order"
+    __table_args__  = (
         UniqueConstraint("module_id", "order", name="unique_module_order"),
     )
 
-    module_id = Column(BIGINT, ForeignKey("modules.id"), primary_key=True)
-    task_id = Column(BIGINT, ForeignKey("tasks.id"), primary_key=True)
-    order = Column(Integer, nullable=False, index=True)
+    module_id       = Column(BIGINT, ForeignKey("modules.id"), primary_key=True)
+    task_id         = Column(BIGINT, ForeignKey("tasks.id"), primary_key=True)
+    order           = Column(Integer, nullable=False, index=True)
 
-    created_at = Column(TIMESTAMP(timezone=True), server_default=func.now())
-    updated_at = Column(TIMESTAMP(timezone=True), onupdate=func.now())
+    created_at      = Column(TIMESTAMP(timezone=True), server_default=func.now())
+    updated_at      = Column(TIMESTAMP(timezone=True), onupdate=func.now())
 
-    task = relationship("Task", back_populates="order_info")
-    module = relationship("Module", back_populates="task_orders")
+    task            = relationship("Task", back_populates="order_info")
+    module          = relationship("Module", back_populates="task_orders")
 
 
 class Language(Base):
-    __tablename__ = "languages"
+    __tablename__   = "languages"
 
     id              = Column(BIGINT, primary_key=True, index=True)
     language        = Column(String(64), nullable=False, index=True)
 
-    created_at = Column(TIMESTAMP(timezone=True), server_default=func.now())
+    created_at      = Column(TIMESTAMP(timezone=True), server_default=func.now())
 
-    tasks = relationship(
-        "Task", secondary="tasks_languages", back_populates="languages"
-    )
+    tasks           = relationship("Task", secondary="tasks_languages", back_populates="languages")
 
 
 class TaskLanguage(Base):
-    __tablename__ = "tasks_languages"
-    __table_args__ = (
-        PrimaryKeyConstraint("task_id", "language_id", name="task_language_pk"),
-    )
+    __tablename__   = "tasks_languages"
+    __table_args__  = (PrimaryKeyConstraint("task_id", "language_id", name="task_language_pk"),)
 
-    task_id = Column(BIGINT, ForeignKey("tasks.id"), nullable=False, index=True)
-    language_id = Column(BIGINT, ForeignKey("languages.id"), nullable=False, index=True)
+    task_id         = Column(BIGINT, ForeignKey("tasks.id"), nullable=False, index=True)
+    language_id     = Column(BIGINT, ForeignKey("languages.id"), nullable=False, index=True)
 
 
 class Solution(Base):
-    __tablename__ = "solutions"
-    __table_args__ = (PrimaryKeyConstraint("user_id", "task_id", name="solution_pk"),)
+    __tablename__   = "solutions"
+    __table_args__  = (PrimaryKeyConstraint("user_id", "task_id", name="solution_pk"),)
 
-    user_id = Column(BIGINT, ForeignKey("users.id"), nullable=False, index=True)
-    task_id = Column(BIGINT, ForeignKey("tasks.id"), nullable=False, index=True)
+    user_id         = Column(BIGINT, ForeignKey("users.id"), nullable=False, index=True)
+    task_id         = Column(BIGINT, ForeignKey("tasks.id"), nullable=False, index=True)
 
     language        = Column(String(64), nullable=False, index=True)
+    current_code	= Column(Text, nullable=False)
     is_solved       = Column(Boolean, nullable=False, default=False, index=True)
     
     created_at      = Column(TIMESTAMP(timezone=True), server_default=func.now())
     updated_at      = Column(TIMESTAMP(timezone=True), onupdate=func.now())
 
-    created_at = Column(TIMESTAMP(timezone=True), server_default=func.now())
-    updated_at = Column(TIMESTAMP(timezone=True), onupdate=func.now())
-
-    user = relationship("User", back_populates="solutions")
-    task = relationship("Task", back_populates="solutions")
-    attempts = relationship(
-        "Attempt", back_populates="solution", cascade="all, delete-orphan"
-    )
+    user            = relationship("User", back_populates="solutions")
+    task            = relationship("Task", back_populates="solutions")
+    attempts        = relationship("Attempt", back_populates="solution", cascade="all, delete-orphan")
 
 
 class Attempt(Base):
@@ -150,10 +138,10 @@ class Attempt(Base):
     id                  = Column(BIGINT, primary_key=True, index=True)
     solution_user_id    = Column(BIGINT, nullable=False)
     solution_task_id    = Column(BIGINT, nullable=False)
-    language            = Column(String(64), nullable=False, index=True)
-    current_code	    = Column(Text, nullable=False)
     message			    = Column(String(128), nullable=False)
 
     created_at          = Column(TIMESTAMP(timezone=True), server_default=func.now())
 
     solution            = relationship("Solution", back_populates="attempts")
+
+# @formatter:on

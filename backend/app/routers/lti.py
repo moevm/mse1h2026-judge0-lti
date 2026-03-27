6d614555f25ff8a4b85e284510212e2dcbc48c58
ShcherbakMaxim <maksim8305@mail.ru>
2026-03-27 18:18:20 +0300

    feat: implement task #52 - save LTI user data to database

--- backend/app/routers/lti.py
+++ backend/app/routers/lti.py
@@ -1,17 +1,22 @@
-from fastapi import APIRouter, Request
+from fastapi import APIRouter, Request, Depends
 from fastapi.responses import RedirectResponse
-import logging
-
-logger = logging.getLogger(__name__)
+from app.services.lti import LtiServer, get_lti_service
 
 router = APIRouter(prefix="/lti", tags=["lti"])
 
 
 @router.post("/launch", summary="LTI Launch Endpoint")
-async def lti_launch(request: Request):
+async def lti_launch(request: Request, service: LtiServer=Depends(get_lti_service)):
     form_data = await request.form()
 
-    print("=== LTI LAUNCH ===")
-    for key, value in form_data.items():
-        print(f"{key}: {value}")
+    user_id = int(form_data.get("user_id"))
+    username = form_data.get("ext_user_username", str(user_id))
+    full_name = form_data.get("lis_person_name_full", f"User {user_id}")
+    roles     = form_data.get("roles", "Student")
+    # print("=== LTI LAUNCH ===")
+    # for key, value in form_data.items():
+    #     print(f"{key}: {value}")
+    service.upsert_user(user_id, username, full_name, roles)
+    print("===LTI====")
+    print(f"id: {user_id}, name: {username}, fullname: {full_name}, roles: {roles}")
     return RedirectResponse(url="http://localhost", status_code=303)

93f18f356fe05efacfebb4826469276207ea3dac
ShcherbakMaxim <maksim8305@mail.ru>
2026-05-03 18:42:08 +0300

    feat(user): implement filtering with search and include_deleted

--- backend/app/repositories/user.py
+++ backend/app/repositories/user.py
@@ -1,8 +1,9 @@
 from fastapi import Depends
-from sqlalchemy import select
+from sqlalchemy import select, or_, func
 from sqlalchemy.orm import Session
-from app.database.models import User
+from app.database.models import User, Solution
 from app.database.database import session_generator
+from app.schemas.user import UserFilter
 
 
 class UserRepository:
@@ -18,6 +19,29 @@ class UserRepository:
 
     def add(self, user: User):
         self.db.add(user)
+
+    def get_all(self, filters: UserFilter) -> list[User]:
+        query = select(User)
+
+        if not filters.include_deleted:
+            query = query.where(User.deleted_at == None)
+
+        if filters.search:
+            s = f"%{filters.search}%"
+            query = query.where(
+                or_(
+                    User.full_name.ilike(s),
+                    User.username.ilike(s),
+                )
+            )
+        return self.db.scalars(query).all()
+
+    def get_solved_count(self, user_id: int) -> int:
+        query = select(func.count()).select_from(Solution).where(
+            Solution.user_id == user_id,
+            Solution.is_solved == True
+        )
+        return self.db.scalar(query)
 
 
 def get_user_repository(db: Session = Depends(session_generator)) -> UserRepository:

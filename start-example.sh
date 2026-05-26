9bc672aa543eac17d4768ab593a120de304510d1
svvtkho <svtokhovad@gmail.com>
2026-05-26 13:29:26 +0300

    fix: docker-compose.yml and start-example

--- start-example.sh
+++ start-example.sh
@@ -3,10 +3,14 @@
 cp .env.example .env
 cp judge0.conf.example judge0.conf
 
+mkdir -p keys
+
 if [ ! -f keys/private.key ]; then
     echo "Generating LTI keys..."
-    python3 backend/generate_keys.py
+    openssl genrsa -out keys/private.key 2048
+    openssl rsa -in keys/private.key -pubout -out keys/public.key
 fi
 
-docker-compose up --build -d
+docker compose up --build -d
+
 echo "Go to http://localhost/lti-key to get your public key for Moodle."
